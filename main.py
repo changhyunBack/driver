3e486e08c578f873d0793e475e503078823c4eff
changhyunBaek <89127795+changhyunBack@users.noreply.github.com>
2023-11-18 19:07:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import numpy as np
 import tensorflow as tf
 import joblib
 from pyparsing import empty
-
+import matplotlib.pyplot as plt # 새로추가 
+plt.rcParams['font.family'] = 'Malgun Gothic'
+# plt.rcParams['font.family']='AppleGothic'
 st.set_page_config(layout="wide")
 # con1 : 제목, 파일 넣기 
 empty1,con1,empty2 = st.columns([0.3,0.4,0.3])
@@ -45,11 +47,14 @@ if file is not None:
    
     df['time'] = pd.to_datetime(df['Timestamp'])
     df['time'] = df['time'].dt.strftime('%H:%M:%S')
+    
+    # chart 
     with conf5:
         if genre:
             st.line_chart(df,x='time', y=genre)
     
-    with con3:
+    # model predict
+    with con4:
         df.drop(columns=['Timestamp','time'],inplace=True)
         x=df.values
         if len(x)==60:
@@ -67,13 +72,12 @@ if file is not None:
         result = pd.DataFrame(prediction)
         result= result.melt()
         result['variable']=result['variable'].map({0:'창현',1:'도윤',2:'현선'})
-        text="probability value"
-        st.markdown(f"<h3 style='margin-left: 285px; margin-right: 20px;'>{text}</h3>", unsafe_allow_html=True)
-        st.bar_chart(result,x='variable',y='value')
-        
+
+
+        # 단일 예측값
         pred= np.argmax(prediction,1)
+        # 예측 확률값 
         pred_prob=float(prediction.ravel()[pred])
-        
         if pred ==0:
             pred='창현'
         elif pred==1:
@@ -82,11 +86,35 @@ if file is not None:
             pred='현선'
         else:
             pred='Known'
+        
+        labels=['창현','도윤','현선']
+        sizes=prediction.ravel()
+        wedgeprops = {'width':0.6, 'linewidth':1, 'edgecolor':'black'}
+        
+        fig1, ax1 = plt.subplots(figsize=(5,5))
+        ax1.pie(sizes,labels=labels,autopct='%1.1f%%',wedgeprops=wedgeprops,pctdistance=0.7,textprops={'fontsize': 12},labeldistance=1.1)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        ax1.set_facecolor('none')
+        st.pyplot(fig1)
+        
+        
+    with con3:
+            # st.write("Uploaded DataFrame:")
+            # #st.write(df.head(12))
+            # st.write(df.head(50).style.set_table_styles([{'selector': 'tr', 'props': [('max-height', '400px'),('max-column','400px')]}]))
+            num_rows_to_display = len(df)
+            st.write(f"Uploaded DataFrame : {num_rows_to_display}")
+            st.write(df.head(num_rows_to_display).style.set_table_styles([{'selector': 'tr', 'props': [('max-height', '400px'), ('max-column', '400px')]}]))
+            st.markdown(f"<h3 style='margin-left: 150px; margin-right: 0px;'> 운전자는 {round(pred_prob * 100,2)}%로 [{pred}] 입니다</h3>", unsafe_allow_html=True)
 
-        st.markdown(f"<h3 style='margin-left: 200px; margin-right: 20px;'> 운전자는 {round(pred_prob * 100,2)}%로 '{pred}' 입니다</h3>", unsafe_allow_html=True)
-        #st.write(f'### 운전자는 "{pred}" 입니다')
-        with con4:
-                st.write("Uploaded DataFrame:")
-                st.write(df.head(7))
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+# sizes = [15, 30, 45, 10]
+# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-    
+# fig1, ax1 = plt.subplots()
+# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+#         shadow=True, startangle=90)
+# ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+# st.pyplot(fig1)
